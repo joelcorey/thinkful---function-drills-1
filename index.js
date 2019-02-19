7412d5a8c0c32c0f5bcf328f91d722a7121ae412
Joel Corey <joelcorey@fastmail.com>
2019-02-19 11:28:16 -0800

    Add 'born in year' to createGreeting

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and I'm ${age}`;
+  const yearOfBirth = 2019 - age;
+  return `Hi, my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}.`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Joel', 35);
 
 console.log(greeting1);
