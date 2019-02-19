24dd69d5619e97ce1aa2fc0e922626baba0c71f5
Joel Corey <joelcorey@fastmail.com>
2019-02-19 11:40:52 -0800

    Try catch, seperate function

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 'use strict';
 
 function createGreeting(name, age) {
-  const yearOfBirth = getYearOfBirth(age);
+  let yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}.`;
 }
 
 function getYearOfBirth(age, currentYear = 2019){
+  if (age <= 0){
+    throw new Error('Age can not be negative');
+  }
   return currentYear - age;
 }
 
-const greeting1 = createGreeting('Joel', 35);
+let greeting1;
+
+try {
+  greeting1 = createGreeting('Joel', 35);
+} catch (err) {
+  console.log(err);
+}
 
 console.log(greeting1);
