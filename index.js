657c03335ca1fc6e04c966d24ff9333da99583e6
Donald Sonn <dsonn@Donalds-MBP.fios-router.home>
2019-02-19 14:19:13 -0500

    return greeting instead of direct log to console

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function createGreeting() {
-  console.log('Hi, my name is Joel and I\'m 35');
+  let hello = 'Hi, my name is Joel and I\'m 35';
+  return hello;
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+
+console.log(greeting1);
