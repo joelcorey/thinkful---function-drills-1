86b6679a4f46f9b950ab31e03cbe1c18765e60b2
Joel Corey <joelcorey@fastmail.com>
2019-02-19 11:32:10 -0800

    Seperate currentYear into function

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function createGreeting(name, age) {
-  const yearOfBirth = 2019 - age;
+  const yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}.`;
+}
+
+function getYearOfBirth(age, currentYear = 2019){
+  return currentYear - age;
 }
 
 const greeting1 = createGreeting('Joel', 35);
